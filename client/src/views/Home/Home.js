70c8bb673fa2387d033998094917d58687da81c2
Willy Deng <wdeng18@ufl.edu>
2020-03-07 19:03:57 -0500

    Implemented a button that inserts dummy info into dummyData

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -8,6 +8,13 @@ import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import SearchRounded from '@material-ui/icons/SearchRounded';
 import ToolDetailPopup from '../../components/ToolDetailPopup';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { makeStyles } from '@material-ui/core/styles';
 
 // Styled components
 const RootContainer = styled.div`
@@ -57,9 +64,39 @@ const dummyData = [
   }
 ];
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(25),
+    },
+  },
+  input: {
+    display: 'none'
+  },
+}));
+
 function Home() {
   const [filteredTools, setFilteredTools] = useState(dummyData);
   const [selectedTool, setSelectedTool] = useState(null);
+  const [open,setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
+  const addToDummy = () => {
+    let obj = {};
+    obj['src'] = logo;
+    obj['keywords'] = ['testing if this works', 'abc'];
+    dummyData.push(obj);
+    setOpen(false);
+  };
+
+  const classes = useStyles();
 
   const filterTools = e => {
     const searchTerm = e.target.value;
@@ -98,6 +135,48 @@ function Home() {
           }}
         />
       </SearchBarForm>
+
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        Upload A New Tool
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title"> 
+        <DialogTitle id="form-dialog-title" >Upload A New Tool</DialogTitle >
+        <DialogContent>
+          <DialogContentText>
+            To upload a new tool, fill in the info below. (Just click upload for demo)
+          </DialogContentText>
+          <div className={classes.root}>
+            <input
+              accept="image/*"
+              className={classes.input}
+              id="contained-button-file"
+              type="file"
+            />
+            <label htmlFor="contained-button-file">
+              <Button variant="contained" color="primary" component="span" >
+                Upload An Image
+              </Button>
+            </label>
+          </div>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="keywords"
+            label="Keywords"
+            type="keywords"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={addToDummy} color="primary">
+            Upload
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <Grid container>
         {filteredTools.map((d, index) => (
           <Grid item xs={4} key={index}>

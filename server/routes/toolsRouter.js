c0f9999f9de68bb7e75d343078c40c1603dc804a
drew-gill <drewgill89@gmail.com>
2020-03-09 15:39:08 -0400

    ToolRouter /api/

--- server/routes/toolsRouter.js
+++ server/routes/toolsRouter.js
@@ -21,25 +21,25 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 //creates a new object in the DB with the upload buffer object (the image with the filepath specified in a file parameter of the request) and the keywords in the request
-toolsRouter.post('/',upload.single('image'), toolController.create);
+toolsRouter.post('/api/',upload.single('image'), toolController.create);
 
 //gets a string output of all of the objects in the database (images represented by binary buffers)
-toolsRouter.get('/', toolController.read);
+toolsRouter.get('/api/', toolController.read);
 
 //removes the object specified by the ID in the parameters of the request from the DB
-toolsRouter.delete('/',toolController.remove);
+toolsRouter.delete('/api/',toolController.remove);
 
 //updates the object represented by the ID parameter with the file in a new given filepath and new keywords
-toolsRouter.put('/', upload.single('image'),toolController.update);
+toolsRouter.put('/api/', upload.single('image'),toolController.update);
 
 //possibly redundant... remove?
-toolsRouter.put('/:id', upload.single('image'),toolController.update);
+toolsRouter.put('/api/:id', upload.single('image'),toolController.update);
 
 //possibly redundant... remove?
-toolsRouter.delete('/:id',toolController.remove);
+toolsRouter.delete('/api/:id',toolController.remove);
 
 //queries for the specific object given by the ID
-toolsRouter.get('/:id',toolController.read);
+toolsRouter.get('/api/:id',toolController.read);
 
 
 module.exports = toolsRouter;

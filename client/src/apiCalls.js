d5d6d86dd448bf257e56ecd1ac8047032aa71086
Zachary Cowan <cz8ch8ry@gmail.com>
2020-03-10 15:37:03 -0400

    Added heroku config variable

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -1,18 +1,8 @@
 import axios from 'axios';
 
 const devUrl = 'http://localhost:5000/api/';
-const prodUrl = '';
 
-const URL = devUrl;
-
-// //todo: add db uri
-// export default {
-//   getData: async () => {
-//     let res = await axios.get('http://localhost:5000/');
-//     console.log(res);
-//     return res.data || [];
-//   }
-// };
+const URL = process.env.API_URL || devUrl;
 
 const readAllTools = async () => {
   const res = await axios.get(URL);

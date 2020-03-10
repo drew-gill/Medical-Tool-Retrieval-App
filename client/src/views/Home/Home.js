3e03ad19eeebc9c5e6aab733aae1bd57fda76445
Zachary Cowan <cz8ch8ry@gmail.com>
2020-03-10 15:31:34 -0400

    Updated filter and display component

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 // Material UI
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import SearchRounded from '@material-ui/icons/SearchRounded';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 // Custom components
 import ToolDetailPopup from '../../components/ToolDetailPopup';
+import FilterAndViewComponent from '../../components/FilterAndViewComponent';
 import AddToolComponent from '../../components/AddToolComponent';
 import { readAllTools, deleteTool, createTool } from '../../apiCalls';
 
@@ -26,35 +23,14 @@ const LoadingContainer = styled.div`
   justify-content: center;
 `;
 
-const SearchBarForm = styled.form`
-  margin: 10px 0px;
-`;
-
-const GridItem = styled(Grid)`
-  &:hover {
-    cursor: pointer;
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-`;
-
-const ToolImage = styled.img`
-  width: 100%;
-  height: 300px;
-  padding: 10px;
-  box-sizing: border-box;
-  object-fit: cover;
-`;
-
 function Home() {
   const [data, setData] = useState(null);
-  const [filteredTools, setFilteredTools] = useState([]);
   const [selectedTool, setSelectedTool] = useState(null);
 
   useEffect(() => {
     const fetch = async () => {
       const res = await readAllTools();
       setData(res);
-      setFilteredTools(res);
     };
     fetch();
   }, []);
@@ -64,9 +40,6 @@ function Home() {
     const newData = data;
     newData.push(newTool);
     setData(newData);
-    const newFilteredTools = filteredTools;
-    newFilteredTools.push(newTool);
-    setFilteredTools(newFilteredTools);
   };
 
   const removeTool = async id => {
@@ -79,29 +52,10 @@ function Home() {
       }
     });
     setData(newData);
-    // Update the data that is displayed
-    const newFilteredTools = [];
-    filteredTools.forEach(d => {
-      if (d._id !== id) {
-        newFilteredTools.push(d);
-      }
-    });
-    setFilteredTools(newFilteredTools);
   };
 
-  const filterTools = e => {
-    const searchTerm = e.target.value;
-    const newData = [];
-    data.forEach(d => {
-      for (let i = 0; i < d.keywords.length; i++) {
-        const word = d.keywords[i];
-        if (word.search(searchTerm) !== -1) {
-          newData.push(d);
-          break;
-        }
-      }
-    });
-    setFilteredTools(newData);
+  const selectTool = item => {
+    setSelectedTool(item);
   };
 
   if (data === null) {
@@ -114,39 +68,9 @@ function Home() {
 
   return (
     <RootContainer>
-      <SearchBarForm noValidate>
-        <TextField
-          onChange={filterTools}
-          label='Search'
-          placeholder='Type to search...'
-          type='search'
-          autoCorrect='off'
-          autoCapitalize='off'
-          autoComplete='off'
-          spellCheck='false'
-          id='search'
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position='start'>
-                <SearchRounded />
-              </InputAdornment>
-            )
-          }}
-        />
-      </SearchBarForm>
+      <FilterAndViewComponent data={data} selectFunction={selectTool} />
 
       <AddToolComponent createFunction={addTool} />
-
-      <Grid container alignContent='stretch'>
-        {filteredTools.map((d, index) => (
-          <GridItem item xs={4} key={d._id} onClick={() => setSelectedTool(d)}>
-            <ToolImage
-              src={`data:image/jpg;base64, ${d.image.toString('base64')}`}
-            />
-          </GridItem>
-        ))}
-      </Grid>
 
       <ToolDetailPopup
         tool={selectedTool}

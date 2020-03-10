5f090d87963b11cf47fa82fbdf0f4ab33b9b61ce
Zachary Cowan <cz8ch8ry@gmail.com>
2020-03-10 15:45:35 -0400

    Potential fix for herku config

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -2,22 +2,24 @@ import axios from 'axios';
 
 const devUrl = 'http://localhost:5000/api/';
 
-const URL = process.env.API_URL || devUrl;
+const getUrl = () => {
+  return process.env.API_URL || devUrl;
+};
 
 const readAllTools = async () => {
-  const res = await axios.get(URL);
+  const res = await axios.get(getUrl());
   return res.data || [];
 };
 
 const deleteTool = async id => {
-  await axios.delete(`${URL}?id=${id}`);
+  await axios.delete(`${getUrl()}?id=${id}`);
 };
 
 const createTool = async (image, keywords) => {
   let formData = new FormData();
   formData.append('image', image);
   formData.append('keywords', JSON.stringify(keywords));
-  const res = await axios.post(URL, formData);
+  const res = await axios.post(getUrl(), formData);
   return res.data;
 };
 

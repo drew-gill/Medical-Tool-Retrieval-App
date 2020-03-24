5526c6f202fafbaa6bdcb917e2514835f2c78675
Willy Deng <wdeng18@ufl.edu>
2020-03-24 16:39:35 -0400

    Change default routing to /Login

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import Home from './views/Home/Home';
 import NotFound from './views/NotFound';
 import NavBar from './components/Header/NavBar';
 
+import Login from "./views/Login/Login";
+
 const App = (props) => {
   return (
     <div>
       {/* <NavBar /> */}
       <Switch>
+        <Route exact path="/Login" component={Login} />
         <Route exact path='/Home' component={Home} />
         <Route exact path='/'>
-          <Redirect to='/Home' />
+          <Redirect to='/Login'/>
         </Route>
         <Route component={NotFound} />
       </Switch>

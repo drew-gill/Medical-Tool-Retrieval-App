13e654bc93eb1660fd187fdd03f7b088819bc72b
Zachary Cowan <cz8ch8ry@gmail.com>
2020-03-26 20:17:46 -0400

    Cleaned up hover functionality

--- client/src/components/FilterAndViewComponent.js
+++ client/src/components/FilterAndViewComponent.js
@@ -6,6 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import SearchRounded from '@material-ui/icons/SearchRounded';
+import Paper from '@material-ui/core/Paper';
 import Skeleton from '@material-ui/lab/Skeleton';
 
 // Styled components
@@ -13,26 +14,33 @@ const SearchBarForm = styled.form`
   margin: 10px 0px;
 `;
 
-const GridItem = styled(Grid)`
+const GridItemContainer = styled.div`
+  width: 100%;
+  height: 300px;
   &:hover {
     cursor: pointer;
-    background-color: rgba(0, 0, 0, 0.1);
+  }
+  position: relative;
+`;
+
+const GridItemHover = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.1);
+  opacity: 0;
+  &:hover {
+    opacity: 1;
   }
 `;
 
 const ToolImage = styled.img`
   width: 100%;
-  height: 300px;
-  padding: 10px;
-  box-sizing: border-box;
+  height: 100%;
   object-fit: cover;
-`;
-
-const SkeletonContainer = styled.div`
-  width: 100%;
-  height: 300px;
-  padding: 10px;
-  box-sizing: border-box;
+  z-index: 3;
 `;
 
 const FilterAndViewComponent = ({ data, selectFunction }) => {
@@ -94,27 +102,25 @@ const FilterAndViewComponent = ({ data, selectFunction }) => {
         />
       </SearchBarForm>
 
-      <Grid container alignContent='stretch'>
+      <Grid container alignContent='stretch' spacing={3}>
         {filteredData.length > 0
           ? filteredData.map((d, index) => (
-              <GridItem
-                item
-                xs={12}
-                sm={6}
-                md={4}
-                lg={3}
-                key={d._id}
-                onClick={() => handleSelect(d)}
-              >
-                <ToolImage
-                  src={`data:image/jpg;base64, ${d.image.toString('base64')}`}
-                />
-              </GridItem>
+              <Grid item xs={12} sm={6} md={4} lg={3} key={d._id}>
+                <GridItemContainer onClick={() => handleSelect(d)}>
+                  <GridItemHover />
+                  <ToolImage
+                    src={`data:image/jpg;base64, ${d.image.toString('base64')}`}
+                  />
+                </GridItemContainer>
+              </Grid>
             ))
           : generateSkeletons().map((skel, index) => (
-              <GridItem item xs={12} sm={6} md={4} lg={3} key={index}>
-                <SkeletonContainer>{skel}</SkeletonContainer>
-              </GridItem>
+              <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                <GridItemContainer>
+                  <GridItemHover />
+                  {skel}
+                </GridItemContainer>
+              </Grid>
             ))}
       </Grid>
     </React.Fragment>

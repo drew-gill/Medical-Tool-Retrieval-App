4f6f0777f648e58b6dda2122bfa9618193a2b1c4
Zachary Cowan <cz8ch8ry@gmail.com>
2020-03-30 15:55:41 -0400

    Integrated authContext with login page

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+
+// Custom components
+import { AuthContext } from '../../Auth';
 
 // Material UI
 import { makeStyles } from '@material-ui/core/styles';
@@ -14,61 +17,84 @@ const LoginContainer = styled.div`
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  text-align: center
+  text-align: center;
 `;
 
 const ButtonContainer = styled.div`
   padding-top: 10px;
-  text-align: center
+  text-align: center;
 `;
 
 const useStyles = makeStyles(theme => ({
   root: {
     '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
+      margin: theme.spacing(1)
+    }
+  }
 }));
 
 const Login = () => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const authContext = useContext(AuthContext);
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const validateForm = () => {
-      return username.length > 0 && password.length > 0;
-    }
+  const validateForm = () => {
+    return username.length > 0 && password.length > 0;
+  };
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-    }
+  const handleSubmit = async event => {
+    event.preventDefault();
+    await authContext.login(username, password);
+  };
 
-    const history = useHistory();
-    const handleRedirect = () => {
-      history.push("/Home");
-    }
+  const history = useHistory();
+  const handleRedirect = () => {
+    history.push('/Home');
+  };
 
-    return (
-      <LoginContainer>
-          LOGIN
-          <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
-            <TextField id="username" label="Username" value={username} autoFocus onChange={e => setUsername(e.target.value)} />
-            <TextField id="password" label="Password" value={password} onChange={e => setPassword(e.target.value)} />
-            <ButtonContainer>
-              <Button disabled={!validateForm()} variant="contained" color='primary'>
-                Sign In
-              </Button>
-              <br></br>
-              <ButtonContainer>or</ButtonContainer>
-              <br></br>
-              <Button onClick={handleRedirect} variant="contained" color='primary'>
-                Continue
-              </Button>
-            </ButtonContainer>
-          </form>
-      </LoginContainer>
-    );
+  return (
+    <LoginContainer>
+      LOGIN
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete='off'
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          id='username'
+          label='Username'
+          value={username}
+          autoFocus
+          onChange={e => setUsername(e.target.value)}
+        />
+        <TextField
+          id='password'
+          label='Password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <ButtonContainer>
+          <Button
+            disabled={!validateForm()}
+            variant='contained'
+            color='primary'
+            type='submit'
+          >
+            Sign In
+          </Button>
+          <br></br>
+          <ButtonContainer>or</ButtonContainer>
+          <br></br>
+          <Button onClick={handleRedirect} variant='contained' color='primary'>
+            Continue
+          </Button>
+        </ButtonContainer>
+      </form>
+    </LoginContainer>
+  );
 };
 
 export default Login;

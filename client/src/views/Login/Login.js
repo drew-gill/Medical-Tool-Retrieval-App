e5b6f63129ebb49f8b0202a9165c3ea9f475b8e7
Willy Deng <wdeng18@ufl.edu>
2020-03-30 23:46:06 -0400

    Style UI to be in line with rest of webapp and added some error handling for failed verification

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -9,10 +9,15 @@ import { AuthContext } from '../../Auth';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Typography from '@material-ui/core/Typography';
 
 const LoginContainer = styled.div`
   width: 300px;
-  height: 300px;
+  height: 450px;
   position: absolute;
   left: 50%;
   top: 50%;
@@ -21,7 +26,8 @@ const LoginContainer = styled.div`
 `;
 
 const ButtonContainer = styled.div`
-  padding-top: 10px;
+  padding-top: 2px;
+  padding-bottom: 2px;
   text-align: center;
 `;
 
@@ -36,27 +42,41 @@ const useStyles = makeStyles(theme => ({
 const Login = () => {
   const classes = useStyles();
   const authContext = useContext(AuthContext);
+  const history = useHistory();
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [retryUsername, setRetryUsername] = useState(false);
+  const [retryPassword, setRetryPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const validateForm = () => {
     return username.length > 0 && password.length > 0;
   };
-
+  
   const handleSubmit = async event => {
     event.preventDefault();
     await authContext.login(username, password);
+    if (history.location.pathname !== '/Login') {
+      console.log("successfuly logged in");
+    }
+    else {
+      setRetryUsername(true);
+      setRetryPassword(true);
+    }
   };
 
-  const history = useHistory();
   const handleRedirect = () => {
     history.push('/Home');
   };
 
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <LoginContainer>
-      LOGIN
+      <Typography variant='h2' gutterBottom={true}>Login</Typography>
       <form
         className={classes.root}
         noValidate
@@ -68,13 +88,39 @@ const Login = () => {
           label='Username'
           value={username}
           autoFocus
-          onChange={e => setUsername(e.target.value)}
+          onChange={(e) => {
+            setUsername(e.target.value);
+            setRetryUsername(false);
+            }
+          }
+          error={retryUsername}
+          helperText={retryUsername ? 'Possible Invalid Username!': ' '}
+          fullWidth
         />
         <TextField
           id='password'
+          type={showPassword ? "text" : "password"}
           label='Password'
           value={password}
-          onChange={e => setPassword(e.target.value)}
+          onChange={(e) => {
+            setPassword(e.target.value)
+            setRetryPassword(false);
+            }
+          }
+          error={retryPassword}
+          helperText={retryPassword ? 'Possible Invalid Password!' : ' '}
+          fullWidth
+          InputProps={{
+            endAdornment:
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }}
         />
         <ButtonContainer>
           <Button
@@ -85,10 +131,20 @@ const Login = () => {
           >
             Sign In
           </Button>
-          <br></br>
-          <ButtonContainer>or</ButtonContainer>
-          <br></br>
-          <Button onClick={handleRedirect} variant='contained' color='primary'>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Typography 
+            ariant='body1'
+          >
+            or
+          </Typography>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button 
+            onClick={handleRedirect} 
+            variant='contained' 
+            color='primary'
+          >
             Continue
           </Button>
         </ButtonContainer>
